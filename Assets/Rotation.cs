e7c85853d2b65422ab49ba7fcb950db1ea59f05b
nakamura001 <polluxtk@gmail.com>
2014-05-11 19:22:04 +0900

    Initial commit.

---
+++ Assets/Rotation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	void Update () {
+		transform.Rotate(new Vector3(0, Time.deltaTime*100f, 0));
+	}
+}
---
+++ Assets/ScreenRecorder.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class ScreenRecorder : MonoBehaviour
+{
+	public int framerate = 30;
+	public int superSize;
+	public bool autoRecord;
+	
+	int frameCount;
+	bool recording;
+	
+	void Start ()
+	{
+		if (autoRecord) StartRecording ();
+	}
+	
+	void StartRecording ()
+	{
+		System.IO.Directory.CreateDirectory ("Capture");
+		Time.captureFramerate = framerate;
+		frameCount = -1;
+		recording = true;
+	}
+
+	IEnumerator Capture (string filePath) {
+		yield return new WaitForEndOfFrame();
+
+		int width = Screen.width;
+		int height = Screen.height;
+		Texture2D tex = new Texture2D (width, height, TextureFormat.ARGB32, false);
+		// Read screen contents into the texture
+		tex.ReadPixels (new Rect(0, 0, width, height), 0, 0);
+		tex.Apply ();
+		
+		// Encode texture into PNG
+		byte[] bytes = tex.EncodeToPNG();
+		Destroy (tex);
+		
+		// For testing purposes, also write to a file in the project folder
+		File.WriteAllBytes(filePath, bytes);
+	}
+
+	void Update ()
+	{
+		if (recording)
+		{
+			if (Input.GetMouseButtonDown (0))
+			{
+				recording = false;
+				enabled = false;
+			}
+			else
+			{
+				if (frameCount > 0)
+				{
+					var name = "Capture/frame" + frameCount.ToString ("0000") + ".png";
+					StartCoroutine(Capture(name));
+				}
+				
+				frameCount++;
+				
+				if (frameCount > 0 && frameCount % 60 == 0)
+				{
+					Debug.Log ((frameCount / 60).ToString() + " seconds elapsed.");
+				}
+			}
+		}
+	}
+	
+	void OnGUI ()
+	{
+		if (!recording && GUI.Button (new Rect (0, 0, 200, 50), "Start Recording"))
+		{
+			StartRecording ();
+			Debug.Log ("Click Game View to stop recording.");
+		}
+	}
+}
